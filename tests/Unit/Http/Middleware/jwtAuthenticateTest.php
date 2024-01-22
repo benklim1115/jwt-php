37672f45d0eab40f0b857b71d848287e0c477532
Ben Klimchock <klimmy1115@gmail.com>
2024-01-22 08:51:27 -0600

    Setting up authentication test

---
+++ tests/Unit/Http/Middleware/jwtAuthenticateTest.php
@@ -0,0 +1,28 @@
+<?php
+
+test("JWT authentication produces the correct response", function () {
+
+    //->ARRANGE
+
+
+    //Request
+
+
+    //Middleware
+
+
+    //RequestHandlerInterface
+
+
+    //->ACT
+
+
+    //Middleware process()
+
+
+    //->ASSERT
+    //expect 401 response
+    //Invalid token header
+
+
+}
