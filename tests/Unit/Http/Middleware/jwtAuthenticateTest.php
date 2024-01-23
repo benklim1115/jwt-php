0d3c4aa9ddfb567369e9ebb99942cad46204d9fa
Ben Klimchock <klimmy1115@gmail.com>
2024-01-23 13:43:19 -0600

    Add remainder of test to see if we get an unauthorized error

--- tests/Unit/Http/Middleware/jwtAuthenticateTest.php
+++ tests/Unit/Http/Middleware/jwtAuthenticateTest.php
@@ -1,32 +1,38 @@
 <?php
 
+use App\Http\Middleware\RequestHandlerInterface;
+
 test("JWT authentication produces the correct response", function () {
 
     //->ARRANGE
-
-
     //Request
     $request = \App\Http\Request::create(
-        string $method,
-        string $uri,
-        array $server [],
+        "GET",
+        "/some/path",
+        //important because this is where we will have our server variables
+        ["HTTP_AUTHORIZATION" => "Bearer some.fake.token"],
     );
 
     //Middleware
+    //need secret key to perform verification
+    $jwtAuth = new JwtAuthenticate("some-secret-key");
 
 
     //RequestHandlerInterface
+    $requestHandler = Mockery::mock(RequestHandlerInterface::class);
 
 
     //->ACT
-
-
     //Middleware process()
+    $response = $jwtAuth->process($request, $requestHandler);
 
 
     //->ASSERT
     //expect 401 response
     //Invalid token header
-
+    expect($response)
+        ->toBeInstanceOf(\App\Http\Response::class)
+        ->and($response->getStatusCode())->toBe(401)
+        ->and($response->getHeaders()["WWW-Authenticate"])->toBe("Bearer error='invalid_token'");
 
 }
